ed34e7be99c4b4488d8fb0a6fa1307cd8c3ad81b
Suchetha.Nambanoor-Kunnath <snk58@mcl287793.ultrahub>
2024-09-02 10:14:17 +0100

    Remove setup_env.sh and update repository

--- setup_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Get the directory path of the current script
-PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-
-# Set PYTHONPATH environment variable
-export PYTHONPATH="$PROJECT_DIR:$PYTHONPATH"
